cc1f60ba6bed4630ae000876b968d2e0201a8fe7
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-20 19:27:10 -0700

    Initializes create-next-app

---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Image from 'next/image';
+//import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Art Supplies Inventory</title>
+      </Head>
+      <h1>Art Supplies Inventory</h1>
+    </div>
+  )
+};
+
+export default Home;
