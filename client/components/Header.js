e068ae78b8d6a898e8568172bee0ea9b2737cdb3
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-21 14:30:33 -0700

    Adds a header component

---
+++ client/components/Header.js
@@ -0,0 +1,18 @@
+// todo: add header styles
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1>
+          Art Supplies Inventory
+        </h1>
+        <p>
+          Providing quality mediums since 1986
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
