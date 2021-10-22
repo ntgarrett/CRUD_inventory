33eb559dfd289abd70fbeda756e9579cebd0e087
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-21 18:21:07 -0700

    Adds API route for deleting product

---
+++ client/pages/api/delete/[id].js
@@ -0,0 +1,9 @@
+import { database } from "../../../config";
+
+export default async function handler({ query: { id }}, res) {
+  const deletedProduct = await fetch(`${database}/${id}`, {
+    method: 'DELETE'
+  }).then(response => response.json());
+  
+  await res.status(200).json(`${JSON.stringify(deletedProduct)} deleted successfully.`)
+};
