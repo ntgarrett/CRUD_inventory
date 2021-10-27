c3945dd99344d171854a447d7d7f847b38e291ba
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-27 01:24:03 -0700

    Displays data in a table and adds styles

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,14 +1,29 @@
-import ProductItem from "./ProductItem";
-
-// todo:  add styles
+import productStyles from "../styles/Product.module.css";
 
 const ProductList = ({ inventory }) => {
   return (
     <>
-      <div>
-        {inventory.map((product) => (
-          <ProductItem key={product.id} product={product} />
-        ))}
+      <div className={productStyles.table}>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>In Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td id={productStyles["category"]}>{product.category}</td>
+                <td id={productStyles["count"]}>{product.count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
