3284c6e5127d2c2e782b5cd6bfd726ad4620c621
Nick Garrett <garrett.nick.96@gmail.com>
2021-11-10 20:04:05 -0800

    Adds a logo and adjusts header styling

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,22 @@
+import Image from "next/image";
+import logo from "../public/logo.png";
 import headerStyles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
     <>
       <div className={headerStyles.container}>
-        <h1 className={headerStyles.title}>
-          Art Supplies Inventory
-        </h1>
+        <div className={headerStyles.heading}>
+          <Image 
+            src={logo}
+            alt="logo"
+            width={40}
+            height={44}
+          />
+          <h1 className={headerStyles.title}>
+            John Doe's Art Supplies
+          </h1>
+        </div>
         <p className={headerStyles.description}>
           Providing quality mediums since 1986
         </p>
