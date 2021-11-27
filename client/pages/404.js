708ebf5037caff17e3fd74d2b68f7d891b3c9b5d
Nick Garrett <garrett.nick.96@gmail.com>
2021-11-26 21:27:33 -0800

    Removes period

--- client/pages/404.js
+++ client/pages/404.js
@@ -3,7 +3,7 @@ import errorStyles from "../styles/ErrorPage.module.css";
 const Error404 = () => {
   return (
     <div className={errorStyles.container}>
-      <h1 className={errorStyles.title}>404 <span>- Page not found.</span></h1>
+      <h1 className={errorStyles.title}>404 <span>- Page not found</span></h1>
     </div>
   );
 };
--- client/pages/500.js
+++ client/pages/500.js
@@ -3,7 +3,7 @@ import errorStyles from "../styles/ErrorPage.module.css";
 const Error500 = () => {
   return (
     <div className={errorStyles.container}>
-      <h1 className={errorStyles.title}>500 <span>- An internal server has occurred.</span></h1>
+      <h1 className={errorStyles.title}>500 <span>- An internal server error has occurred</span></h1>
     </div>
   );
 };
