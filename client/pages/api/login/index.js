f83673aba9405cbe21cbc8a30e744e2957a7c99c
Nick Garrett <garrett.nick.96@gmail.com>
2021-11-26 21:36:56 -0800

    Adds a login api route

---
+++ client/pages/api/login/index.js
@@ -0,0 +1,30 @@
+import { withSessionRoute } from "../../../lib/withSession";
+import { database } from "../../../config";
+
+export default withSessionRoute(loginRoute);
+
+async function loginRoute(req, res) {
+  if (Object.keys(req.body).length) {
+    const authenticatedUser = await fetch(`${database}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    }).then(response => response.json());
+
+    if (Object.keys(authenticatedUser).length) {
+      req.session.user = {
+        id: authenticatedUser.userId,
+        admin: authenticatedUser.administrator
+      };
+
+      await req.session.save();
+      res.json({ success: true, message: 'Logged in' });
+    } else {
+      res.json({ success: false, message: 'Incorrect username and/or password' });
+    }
+  } else {
+    res.send('Incorrect username and/or password');
+  }
+}
