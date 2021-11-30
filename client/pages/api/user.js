a1ce7337a642a11c794007bca197d2e65d501d08
Nick Garrett <garrett.nick.96@gmail.com>
2021-11-30 14:02:06 -0800

    Adds additional user data to session

--- client/pages/api/user.js
+++ client/pages/api/user.js
@@ -1,18 +1,33 @@
 import { withSessionRoute } from "../../lib/withSession";
+import { database } from "../../config";
 
 export default withSessionRoute(userRoute);
 
 async function userRoute(req, res) {
-	if (req.session.user) {
-		res.json({
-			...req.session.user,
-			isLoggedIn: true,
-		});
-	} else {
-		res.json({
-			isLoggedIn: false,
-			userId: null,
-			isAdmin: false
-		});
+	try {
+		if (req.session.user.userId) {
+			const additionalUserData = await fetch(`${database}/user/${req.session.user.userId}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}).then(response => response.json());
+
+			res.json({
+				...req.session.user,
+				firstName: additionalUserData.user.first_name,
+				lastName: additionalUserData.user.last_name,
+				birthDate: additionalUserData.user.birth_date,
+				hireDate: additionalUserData.user.hire_date
+			});
+		} else {
+			res.json({
+				isLoggedIn: false,
+				userId: null,
+				isAdmin: false
+			});
+		}
+	} catch (error) {
+		res.json(error.message);
 	}
 } 
